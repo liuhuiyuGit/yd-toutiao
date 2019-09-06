a48042faac055318d530197eaaafe7346ab10a16
liuhuiyu <328609918@qq.com>
2019-09-06 21:07:41 +0800

    设置home的子路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../views/Home.vue'
+import Tabbar from '@/views/Tabbar.vue'
+import Home from '@/views/home/index.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: Tabbar,
+      children: [
+        {
+          name: 'home',
+          path: '',
+          component: Home
+        }
+      ]
     },
     {
       path: '/login',
@@ -17,7 +24,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+      component: () => import(/* webpackChunkName: "about" */ '@/views/login.vue')
     }
   ]
 })
