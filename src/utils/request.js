fc29f05b33f2b4b4dea1407158909edaaf5e1c55
liuhuiyu <328609918@qq.com>
2019-09-06 19:49:40 +0800

    处理大数字

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,21 @@
 import axios from 'axios'
-
+import JSONbig from 'json-bigint'
 // 创建一个axios实例 设置不同的baseURL
 const instance = axios.create({
   timeout: 5000,
   baseURL: 'http://ttapi.research.itcast.cn'
 })
+
+instance.defaults.transformResponse = [function (data) {
+  try {
+    // data 数据可能不是标准的 JSON 格式字符串，否则会导致 JSONbig.parse(data) 转换失败报错
+    return JSONbig.parse(data)
+  } catch (err) {
+    // 无法转换的数据直接原样返回
+    return data
+  }
+}]
+
 // 请求拦截
 instance.interceptors.request.use(function (config) {
   return config
