8276e595181c1f9d859848b82b7f3816ed9b74d6
liuhuiyu <328609918@qq.com>
2019-09-06 21:08:45 +0800

    设置了tabbar

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -1,11 +1,22 @@
 <template>
-<div>
-  home
-</div>
+  <div>
+    <router-view></router-view>
+
+    <van-tabbar route>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/qa" icon="search">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
+
 <script>
 export default {
+  name: 'Tabbar'
 }
 </script>
+
 <style>
+
 </style>
