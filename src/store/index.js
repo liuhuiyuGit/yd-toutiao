363a08fb92b36a08456cef2344de5f135cf7156f
liuhuiyu <328609918@qq.com>
2019-09-06 20:41:06 +0800

    把token值存入到本地

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as storageTools from '@/utils/localStorage'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: ''
+    user: storageTools.getItem('user')
   },
   mutations: {
     setUser (stste, user) {
       stste.user = user
+      storageTools.setItem('user', user)
     }
   },
   actions: {
