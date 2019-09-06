e12956c74f05b79795e269db74a2258ae0e81ab3
liuhuiyu <328609918@qq.com>
2019-09-06 20:10:35 +0800

    登录按钮结构搭建

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+import './styles/index.less'
 Vue.use(Vant)
 
 Vue.config.productionTip = false
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,8 +1,33 @@
 <template>
-  <div>登录</div>
+  <div>
+    <!-- 标题 -->
+    <van-nav-bar title="标题"/>
+    <!-- 输入框 -->
+    <van-cell-group>
+      <van-field v-model="value" left-icon="phone-o" placeholder="请输入手机号"/>
+      <van-field v-model="value" left-icon="envelop-o" placeholder="请输入验证码">
+        <van-button slot="button" size="small" class="btn" type="default">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class="information">
+      <van-button class="informationbtn" type="info">登录</van-button>
+    </div>
+  </div>
 </template>
 <script>
 export default {}
 </script>
-<style>
+<style scoped lang="less">
+.btn{
+  border-radius: 40px;
+  background-color: #ededed;
+  color: #666;
+}
+.information{
+  padding: 20px;
+  .informationbtn{
+    width: 100%;
+  }
+}
 </style>
