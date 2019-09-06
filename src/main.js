0f8bcc88d81443fcdafc62f570aff77f04a10d88
liuhuiyu <328609918@qq.com>
2019-09-06 20:48:16 +0800

    登录表单验证

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+import VeeValidate from 'vee-validate'
 import 'vant/lib/index.css'
 import './styles/index.less'
 Vue.use(Vant)
-
+Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login.vue
+++ src/views/login.vue
@@ -4,8 +4,20 @@
     <van-nav-bar title="标题"/>
     <!-- 输入框 -->
     <van-cell-group>
-      <van-field v-model="user.mobile" left-icon="phone-o" placeholder="请输入手机号"/>
-      <van-field v-model="user.code" left-icon="envelop-o" placeholder="请输入验证码">
+      <van-field
+      v-validate="'required|digits:11'"
+      name="mobile"
+      :error-message="errors.first('mobile')"
+      v-model="user.mobile"
+      left-icon="phone-o"
+      placeholder="请输入手机号"/>
+      <van-field
+        v-validate="'required|digits:6'"
+        name="code"
+        :error-message="errors.first('code')"
+      v-model="user.code"
+      left-icon="envelop-o"
+      placeholder="请输入验证码">
         <van-button slot="button" size="small" class="btn" type="default">发送验证码</van-button>
       </van-field>
     </van-cell-group>
@@ -31,6 +43,11 @@ export default {
     ...mapMutations(['setUser']),
     async handleLogin () {
       try {
+        const valid = await this.$validator.validate()
+        if (!valid) {
+          this.loading = false
+          return
+        }
         const data = await login(this.user)
         this.setUser(data)
         this.$router.push('/')
@@ -40,6 +57,25 @@ export default {
         console.log(err)
       }
     }
+  },
+  created () {
+    // 配置VeeValidate的自定义验证信息
+    const dict = {
+      custom: {
+        // 验证的文本框
+        mobile: {
+          // 验证规则失败之后的提示信息
+          required: '请输入手机号码',
+          digits: '手机号码必须是11位的数字'
+        },
+        code: {
+          required: '请输入验证码',
+          digits: '验证码必须是6位的数字'
+        }
+      }
+    }
+    // or use the instance method
+    this.$validator.localize('custom', dict)
   }
 }
 </script>
