3002b17e899fa2fc779c7955aa35dd41e5b39a39
liuhuiyu <328609918@qq.com>
2019-09-07 12:34:41 +0800

    上拉更新列表样式布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <!-- 标题 -->
-    <van-nav-bar title="标题"/>
+    <van-nav-bar fixed title="标题"/>
     <!-- top导航栏 -->
     <van-tabs>
-      <van-tab
-      v-for="item in channels"
-      :key = "item.id"
-      :title="item.name">内容</van-tab>
+      <van-tab v-for="item in channels" :key="item.id" :title="item.name">
+          <!-- 上拉更新列表 -->
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+          <van-cell v-for="item in list" :key="item" :title="item"/>
+        </van-list>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
@@ -16,7 +18,10 @@ import { getDefaultOrUserChannels } from '../../api/channel.js'
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      list: [],
+      loading: false,
+      finished: false
     }
   },
   methods: {
@@ -25,6 +30,22 @@ export default {
       const data = await getDefaultOrUserChannels()
       console.log(data)
       this.channels = data.channels
+    },
+    // 上拉更新列表
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
     }
   },
   created () {
@@ -33,5 +54,20 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped lang='less'>
+.van-tabs {
+  /deep/ .van-tabs__wrap {
+    position: fixed;
+    top: 46px;
+    left: 0;
+    z-index: 100;
+  }
+  /deep/ .van-tabs__content {
+    margin-top: 90px;
+    margin-bottom: 50px;
+  }
+}
+.close {
+  float: right;
+}
 </style>
